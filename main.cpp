5bdf106d06824b43848bfc67ccc7e7e512255af4
Kyufujin <bindabartek@gmail.com>
2022-09-01 17:45:08 +0200

    display existing heightmap & base heightmap asset

--- main.cpp
+++ main.cpp
@@ -1,28 +1,64 @@
 #include "raylib.h"
 
-
-int main(void)
+int main()
 {
-
+    // Init
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Generated Height Map");
 
-    SetTargetFPS(60);               // Set our app to run at 60 frames-per-second
+    // Define custom camera
+    Camera camera = { { 18.0f, 18.0f, 18.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
 
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    Image image = LoadImage("../heightmap.png");             // Load heightmap image (RAM), for now lets use some random heightmap
+    Texture2D texture = LoadTextureFromImage(image);                // Convert image to texture (VRAM)
+
+    Mesh mesh = GenMeshHeightmap(image, (Vector3){ 16, 8, 16 });    // Generate heightmap mesh (RAM and VRAM)
+    Model model = LoadModelFromMesh(mesh);                          // Load model from generated mesh
+
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;// Set map diffuse texture
+    Vector3 mapPosition = { -8.0f, 0.0f, -8.0f };                   // Define model position
+
+    UnloadImage(image);                     // Unload heightmap image from RAM, already uploaded to VRAM
+
+    SetCameraMode(camera, CAMERA_ORBITAL);  // Set an orbital camera mode
+
+    SetTargetFPS(60);                       // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main loop of the app
+    while (!WindowShouldClose())            // Detect window close button or ESC key
     {
+        // Update
+        UpdateCamera(&camera);        
+
+        // Draw       
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Window test", 190, 200, 20, LIGHTGRAY);
+            BeginMode3D(camera);
+
+                DrawModel(model, mapPosition, 1.0f, RED);
+
+                DrawGrid(20, 1.0f);
+
+            EndMode3D();
+
+            DrawTexture(texture, screenWidth - texture.width - 20, 20, WHITE);
+            DrawRectangleLines(screenWidth - texture.width - 20, 20, texture.width, texture.height, GREEN);
+
+            DrawFPS(10, 10);
 
         EndDrawing();
     }
 
-    CloseWindow();        // Close window and OpenGL context
+    // De-Initialization
+    UnloadTexture(texture);     // Unload texture
+    UnloadModel(model);         // Unload model
+
+    CloseWindow();              // Close window and OpenGL context
 
     return 0;
 }
